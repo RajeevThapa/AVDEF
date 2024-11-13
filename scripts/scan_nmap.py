f6880b050ff9976c532e192be349364fa1777872
Rajeev Thapa <101322664+RajeevThapa@users.noreply.github.com>
2024-11-13 22:52:17 +0100

    Update scan_nmap.py

--- scripts/scan_nmap.py
+++ scripts/scan_nmap.py
@@ -1,20 +1,15 @@
 import os
 
 def run_nmap(target):
-    # Remove 'http://' or 'https://' prefixes, if present, and trailing slash
     target = target.replace('http://', '').replace('https://', '').rstrip('/')
-
-    # Create a safe filename by replacing characters that are invalid in filenames
     safe_target = target.replace(':', '_').replace('/', '_')
-
-    # Get the workspace environment variable (Jenkins workspace)
-    output_file = os.path.join(os.getenv('WORKSPACE', '.'), "scans", "nmap", f"{safe_target}_nmap.txt")
-
-    # Run the Nmap scan using os.system
+    output_dir = os.getenv('NMAP_OUTPUT_DIR', './scans/nmap/')
+    os.makedirs(output_dir, exist_ok=True)
+    output_file = os.path.join(output_dir, f"{safe_target}_nmap.txt")
+    
     os.system(f"nmap -sV --script vuln {target} -oN {output_file}")
     print(f"Nmap scan completed. Results saved in {output_file}")
 
 if __name__ == "__main__":
-    # Read the target URL from the environment variable (passed by Jenkins pipeline)
-    target = os.getenv('TARGET_URL', 'http://testhtml5.vulnweb.com')  # Default if not provided
+    target = os.getenv('TARGET_URL', 'http://testhtml5.vulnweb.com')
     run_nmap(target)
